c78d09bf32f2081f08e67d85c5fff21673f574be
seifhosam <103901135+seifhosam@users.noreply.github.com>
2022-06-08 00:03:07 +0200

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,33 @@
+<?php
+include "header.php";
+?>
+<html>
+    <head>
+            <meta charset="utf-8">
+            <title>Home</title>
+            <link rel="stylesheet" type="text/css" href="Home.css">
+                      
+            <img src="11.jpeg" alt="sayo" height="900" width="1695" >   
+            <div class="logo">
+            <img src="logo.png" alt="sayo"  height="160" width="150" >  
+            </div>
+            <div class="love">
+            <p> We get what you love </p>
+            </div> <!-- aflet paragraph--> 
+            <div class="fav">
+            <p> Get all your favorite dishes delivered in minutes </p>
+            </div> <!-- aflet paragraph--> 
+           
+            <form action="">  <!-- link el login--> 
+            <input class="button button1" value=" Login" type="submit"> 
+            </form>
+           
+            <form action="">  <!-- link el sign up--> 
+            <input class="button button2" value="  Sign up" type="submit"> 
+            </form>
+           
+    </head>
+<body>
+
+</body>
+</html>
