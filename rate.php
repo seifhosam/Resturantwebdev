bce49b7358fb0b5d45ed68c64a3920fb7be03287
alyyasser2 <104033015+alyyasser2@users.noreply.github.com>
2022-06-10 20:18:38 +0200

    Update rate.php

--- rate.php
+++ rate.php
@@ -1,39 +1,5 @@
-<style>
-
-@import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
-
-/* Styling h1 and links
-––––––––––––––––––––––––––––––––– */
-
-body
-{
-  background: #4a4a4c !important;
-}
-
-.starrating > input {display: none;}  /* Remove radio buttons */
-
-.starrating > label:before { 
-  content: "\f005"; /* Star */
-  margin: 2px;
-  font-size: 2em;
-  font-family: FontAwesome;
-  display: inline-block; 
-}
-
-.starrating > label
-{
-  color: #222222; /* Start color when not clicked */
-}
-
-.starrating > input:checked ~ label
-{ color: #ffca08 ; } /* Set yellow color when star checked */
-
-.starrating > input:hover ~ label
-{ color: #ffca08 ;  } /* Set yellow color when star hover */
-
-
-</style>  
 <?php
+
 session_start();
 $status="";
 if (isset($_POST['action']) && $_POST['action']=="remove"){
@@ -48,15 +14,33 @@ if (isset($_POST['action']) && $_POST['action']=="remove"){
 }
 ?>
 <html>
+<body>
+  
+
+<head>
+   <!-- default styles -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+   <link href="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/css/star-rating.min.css" media="all" rel="stylesheet" type="text/css" />
+
+   <!-- with v4.1.0 Krajee SVG theme is used as default (and must be loaded as below) - include any of the other theme CSS files as mentioned below (and change the theme property of the plugin) -->
+   <link href="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/themes/krajee-svg/theme.css" media="all" rel="stylesheet" type="text/css" />
+
+   <!-- important mandatory libraries -->
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/js/star-rating.min.js" type="text/javascript"></script>
+
+   <!-- with v4.1.0 Krajee SVG theme is used as default (and must be loaded as below) - include any of the other theme JS files as mentioned below (and change the theme property of the plugin) -->
+   <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/themes/krajee-svg/theme.js"></script>
+
+   <!-- optionally if you need translation for your language then include locale file as mentioned below (replace LANG.js with your own locale file) -->
+   <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/js/locales/LANG.js"></script>
+</head>
     <?php
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "test1";
-
-  
   $conn = mysqli_connect($servername, $username, $password, $dbname);
-
     ?>
 <head>
 <title>Rating</title>
@@ -74,7 +58,6 @@ $cart_count = count(array_keys($_SESSION["shopping_cart"]));
 
 <?php
 }
-
 ?>
 
 <div class="cart">
@@ -84,6 +67,7 @@ if(isset($_SESSION["shopping_cart"])){
 }   
 ?>	
 <table class="table">
+  
 <tbody>
 <tr>
 <td></td>
@@ -97,23 +81,10 @@ foreach ($_SESSION["shopping_cart"] as $product){
 <tr>
 <td><img src='<?php echo $product["image"]; ?>' width="50" height="40" /></td>
 <td><?php echo $product["name"]; ?><br />
-<td ><form action="" method="get"> <div class="container">
-        <div class="starrating risingstar d-flex justify-content-center flex-row-reverse">
-            <input type="radio" id="star5" name="rate" value="5" /><label for="star5" title="5 star">5</label>
-            <input type="radio" id="star4" name="rate" value="4" /><label for="star4" title="4 star">4</label>
-            <input type="radio" id="star3" name="rate" value="3" /><label for="star3" title="3 star">3</label>
-            <input type="radio" id="star2" name="rate" value="2" /><label for="star2" title="2 star">2</label>
-            <input type="radio" id="star1" name="rate" value="1" /><label for="star1" title="1 star">1</label>
-
-        </div>
+<td > <label for="input-7-sm" class="control-label"></label>
+        <input id="input-7-sm" class="rating rating-loading" value="" data-min="0" data-max="5" data-step="0.5" data-size="sm"><hr/>
+      
   </div> </form><br />
-
-  <?php
-
-
-  
-  ?>
-<form method='post' action=''>
 <td>
 </tr>
 <?php
@@ -121,14 +92,12 @@ foreach ($_SESSION["shopping_cart"] as $product){
 ?>
 <tr>
 <td colspan="5" align="right">
-<form action="checkout.php" style="margin:10px 230px" ><button style="color:red "><b>Done</b></button></form>
+<form action="Beef.php" style="margin:10px 230px" >  <button style="color:rgb(22, 22, 22); background-color:orange "><b>Done</b></button</form>
 </td>
 </tr>
 </tbody>
 </table>		
-  <?php
-//}
-?>
+
 </div>
 
 <div style="clear:both;"></div>
@@ -136,9 +105,25 @@ foreach ($_SESSION["shopping_cart"] as $product){
 <div class="message_box" style="margin:10px 0px;">
 <?php echo $status; ?>
 </div>
-<br /><br />
+<br/><br />
 </div>
 </body>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
+<script>
+        $(document).ready(function(){
+            $('#input-3').rating({displayOnly: true, step: 0.5});
+            $('#input-5').rating({clearCaption: 'No stars yet'});
+            $('#input-8').rating({rtl: true, containerClass: 'is-star'});
+            $('#input-9').rating();
+        });
+</script>
+</body>
+<style>
+  body{
+background-color:rgb(22, 22, 22);
+color:orange;
+  }
+  td{
+    color:orange
+  }
+</style>
 </html>
