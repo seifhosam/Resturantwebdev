039dadd3d122884f5b0d48b10f015e6d30fc80de
youssefmahmoud568 <102719474+youssefmahmoud568@users.noreply.github.com>
2022-06-10 18:38:11 +0200

    Add files via upload

---
+++ cashirlogin.php
@@ -0,0 +1,19 @@
+
+<?php
+error_reporting(E_ERROR | E_PARSE);
+include"connect.php";
+
+//cashier login 
+if(isset($_POST['login'])) {
+	$sql = mysqli_query($conn,
+	"SELECT * FROM cashier WHERE Name='"
+	. $_POST["username"] . "' AND
+	password='" . $_POST["pwd"] . "' ");
+	$num = mysqli_num_rows($sql);
+	if($num > 0) {
+		$row = mysqli_fetch_array($sql);
+		header("location:contact_us.php");
+		exit();
+	}
+}
+?>
