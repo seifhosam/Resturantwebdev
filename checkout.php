24a86b562588af35417c4ce5441ed45a7a1b19b0
alyyasser2 <104033015+alyyasser2@users.noreply.github.com>
2022-06-10 20:19:58 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -105,37 +105,51 @@ top:380;
     font-size: 14px;
     font-weight: 600;
 }
+.p{
+  font-size:25px;
+  margin-right:240px;
+  
+}
+
+.radiobtn{
+  margin-right:25px;
+}
     </style>
+    <form method="post" action="editorder.php" >
 <div class="container p-0">
         <div class="card px-4">
             <p class="h8 py-3">Payment Details</p>
+            
+
+
             <div class="row gx-3">
                 <div class="col-12">
                     <div class="d-flex flex-column">
                         <p class="text mb-1">Person Name</p>
-                        <input class="form-control mb-3" type="text" placeholder="Name" >
+                        <input class="form-control mb-3" type="text" placeholder="Name" required="">
                     </div>
                 </div>
                 <div class="col-12">
                     <div class="d-flex flex-column">
                         <p class="text mb-1">Card Number</p>
-                        <input class="form-control mb-3" type="text" placeholder="1234 5678 435678">
+                        <input class="form-control mb-3" type="text" placeholder="1234 5678 435678" required="">
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="d-flex flex-column">
                         <p class="text mb-1">Expiry</p>
-                        <input class="form-control mb-3" type="text" placeholder="MM/YYYY">
+                        <input class="form-control mb-3" type="text" placeholder="MM/YYYY" required=""><br>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="d-flex flex-column">
                         <p class="text mb-1">CVV/CVC</p>
-                        <input class="form-control mb-3 pt-2 " type="password" placeholder="***">
+                        <input class="form-control mb-3 pt-2 " type="password" placeholder="***" required="">
                     </div>
                 </div>
-                <button type='submit' class='button'> Confrim </button>
+                <button type='submit' class='button'  required=""> Confrim </button>
             </div>
         </div>
     </div>
+    </form>
 </html>
