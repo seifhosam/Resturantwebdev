389f7b62d654c23f6733572415f907acecca0691
Seif hosam <103901135+seifhosam@users.noreply.github.com>
2022-06-10 20:44:10 +0200

    Add files via upload

---
+++ About.php
@@ -0,0 +1,32 @@
+<?php 
+include "homeheader.php";
+?>
+<html>
+<link rel="stylesheet" type="text/css" href="About.css">
+<div class="photo">
+<img src="resturant.jpeg" alt="sayo" width="650" height="450" >   
+</div>
+<div class="logo">
+<a href="Home.php" ><img src="logo.png" alt="alternate_text"  height="160" width="150" >  </a>
+</div>
+<h1> About US </h1>
+<h2> Since our modest beginnings in 2005 with a little space <br> 
+     in Toronto’s  stylish Yorkville locale, Grand‘s<br> 
+     development has been enlivened with the energy to cook <br> 
+     and serve solid, Indian-roused takeout food. In contrast<br> 
+     to other Indian eateries, Grand Resturant was made <br> 
+     with the explicit expectation to appear as something else.<br> 
+     We realize numerous individuals love Indian sustenance,<br> 
+     yet a large number of them loathe or are unconscious <br> 
+     of the regularly unfortunate fixings that make <br> 
+     run-of-the-mill Indian nourishment taste so great.<br> 
+     Our menu highlights things that utilization the sound and<br> 
+     fragrant flavors, however, forgets the stuffing ghee, <br> 
+     spread, oil, and overwhelming cream. Grand Resturant <br> 
+     has developed to incorporate four superb takeout areas<br> 
+     in Toronto with additional to come sooner rather than <br> 
+     later. Our group takes pride in the way that we can <br> 
+     furnish our new and faithful clients with extraordinary <br> 
+     tasting Indian-roused nourishment that is not normal for <br> 
+     that at some other Indian eatery you visit.</h2>
+</html>
